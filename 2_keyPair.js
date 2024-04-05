3ba39bd72e3bf40fff9f010151a225a346059187
krishna agrawal <agrawalkrishna522@gmail.com>
2024-04-06 04:44:45 +0530

    keypair generation, basic reading data, transaction

---
+++ 2_keyPair.js
@@ -0,0 +1,16 @@
+import "dotenv/config"
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(`The keypair is: `, keypair);
+console.log(`---------------------------------------------------------------`);
+
+
+console.log(`The public key is: `, keypair.publicKey);
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`---------------------------------------------------------------`);
+
+
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`---------------------------------------------------------------`);
---
+++ 3_basic_connection.js
@@ -0,0 +1,9 @@
+import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));  //mainnet-beta, testnet, devnet
+const address = new PublicKey('HWrVDR5YDF5ZXrvEuSNqPf6ofK1ApMkFxpDJ2fbTFGNb');
+const balance = await connection.getBalance(address);
+
+console.log(`The balance of the account at ${address} is ${balance} Lamports`); 
+console.log(`The balance of the account at ${address} is ${balance/LAMPORTS_PER_SOL} solana`); 
+console.log(`✅ Finished!`)
