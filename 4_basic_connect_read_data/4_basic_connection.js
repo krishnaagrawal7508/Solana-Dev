c579107cf67a12adfdf40280135c0450780926b7
krishna agrawal <agrawalkrishna522@gmail.com>
2024-04-07 17:14:04 +0530

    basic changes

--- 4_basic_connect_read_data/4_basic_connection.js
+++ 4_basic_connect_read_data/4_basic_connection.js
@@ -1,7 +1,8 @@
 import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 const connection = new Connection(clusterApiUrl("devnet"));  //mainnet-beta, testnet, devnet
-const address = new PublicKey('HWrVDR5YDF5ZXrvEuSNqPf6ofK1ApMkFxpDJ2fbTFGNb');
+const address = new PublicKey('DrwbtVMaxvvNisf4ZRtVUwHvbanRF3FuQNhEnU65wWWC');
+// const address = new PublicKey('HWrVDR5YDF5ZXrvEuSNqPf6ofK1ApMkFxpDJ2fbTFGNb');
 const balance = await connection.getBalance(address);
 
 console.log(`The balance of the account at ${address} is ${balance} Lamports`); 
